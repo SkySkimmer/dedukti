edf04f855d3639950cfdeb379bd055e209ade70c
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2015-04-30 16:56:22 +0200

    Unification using disagreement pair logic

--- kernel/unifier.ml
+++ kernel/unifier.ml
@@ -18,9 +18,11 @@ let pp_metainfo out = function
   | MetaType (ctx,n)    -> Printf.fprintf out "%a |- ?_%i : *" pp_context ctx n
   | MetaSort (ctx,n)    -> Printf.fprintf out "%a |- ?_%i sort" pp_context ctx n
 
-type problem = { cpt:int; decls: metainfo list; defs: S.t; }
+type disagreement = context*term*term
 
-let empty = { cpt=0; decls=[]; defs=S.identity; }
+type problem = { cpt:int; decls: metainfo list; defs: S.t; pairs: disagreement list; }
+
+let empty = { cpt=0; decls=[]; defs=S.identity; pairs=[]; }
 
 let pp_problem out pb = Printf.fprintf out "cpt=%i;\n%a\n%a\n" pb.cpt (pp_list "\n" pp_metainfo) pb.decls S.pp pb.defs
 
@@ -44,9 +46,9 @@ let new_meta ctx l s c pb =
   let mj = mk_Meta l s pb.cpt substj in
   match c with
     | CTerm ty -> 
-          (mj,{cpt=pb.cpt+1; decls=(MetaDecl (ctx,pb.cpt,ty))::pb.decls; defs=pb.defs;})
-    | CType -> (mj,{cpt=pb.cpt+1; decls=(MetaType (ctx,pb.cpt))::pb.decls; defs=pb.defs;})
-    | CSort -> (mj,{cpt=pb.cpt+1; decls=(MetaSort (ctx,pb.cpt))::pb.decls; defs=pb.defs;})
+          (mj,{cpt=pb.cpt+1; decls=(MetaDecl (ctx,pb.cpt,ty))::pb.decls; defs=pb.defs; pairs=pb.pairs;})
+    | CType -> (mj,{cpt=pb.cpt+1; decls=(MetaType (ctx,pb.cpt))::pb.decls; defs=pb.defs; pairs=pb.pairs;})
+    | CSort -> (mj,{cpt=pb.cpt+1; decls=(MetaSort (ctx,pb.cpt))::pb.decls; defs=pb.defs; pairs=pb.pairs;})
 
 let rec accessible n pb = function
   | Kind | Type _ | DB _ | Const _ | Hole _ -> false
@@ -60,14 +62,14 @@ let rec accessible n pb = function
 
 let set_meta n t pb = if accessible n pb t
   then false,pb
-  else true,{cpt=pb.cpt; decls=pb.decls; defs=S.add pb.defs n t}
+  else true,{cpt=pb.cpt; decls=pb.decls; defs=S.add pb.defs n t; pairs=pb.pairs;}
 
 let set_decl d pb = let n = match d with | MetaDecl (_,n,_) | MetaType (_,n) | MetaSort (_,n) -> n in
   let rec aux s = function
     | MetaDecl (_,m,_) :: tl | MetaType (_,m) :: tl | MetaSort (_,m) :: tl when n=m -> List.rev_append s (d::tl)
     | d' :: tl -> aux (d'::s) tl
     | [] -> assert false
-  in (),{cpt=pb.cpt; decls=aux [] pb.decls; defs=pb.defs;}
+  in (),{cpt=pb.cpt; decls=aux [] pb.decls; defs=pb.defs; pairs=pb.pairs;}
 
 (* We need to infer a type for gamma |- ?j[sigma].
    If delta |- ?j : t in the context we check gamma |- sigma : delta then return sigma(t)
@@ -92,26 +94,42 @@ let meta_constraint = function
 
 let whnf sg t pb = (S.whnf sg pb.defs t,pb)
 
+let rec gen_pair sg ctx t1 t2 : disagreement list option t = whnf sg t1 >>= fun t1' -> whnf sg t2 >>= fun t2' ->
+  match t1', t2' with
+    | Kind, Kind | Type _, Type _ -> return (Some [])
+    | DB (_,n,_), DB (_,m,_) when (n=m) -> return (Some [])
+    | Const (_,m,v), Const (_,m',v') when (ident_eq v v' && ident_eq m m') -> return (Some [])
+    | Pi (_,_,a,b), Pi (_,_,a',b') -> gen_pair sg ctx a a' >>= begin function
+        | Some l -> gen_pair sg ctx b b' >>= begin function
+            | Some l' -> return (Some (List.append l l'))
+            | None -> return None
+            end
+        | None -> return None
+        end
+    | Lam (l,x,Some a,b), Lam (_,_,_,b') | Lam (_,_,_,b), Lam (l,x,Some a,b') -> gen_pair sg ((l,x,a)::ctx) b b'
+    | Meta (_,_,n,_), Meta (_,_,m,_) -> set_meta n t2' >>= (function | true -> return true | false -> set_meta m t1') >>= (function
+        | true -> return (Some []) | false -> return None)
+    | Meta (_,_,n,_), t | t, Meta (_,_,n,_) -> set_meta n t >>= (function | true -> return (Some []) | false -> return None)
+(*
+    | App
+*)
+    | _, _ -> if Reduction.are_convertible sg t1' t2'
+      then return (Some [])
+      else return None
+
+let add_pairs l pb = (),{pb with pairs=List.append pb.pairs l}
+
 let unify sg ctx t c =
-  let rec aux ctx t1 t2 = whnf sg t1 >>= fun t1' -> whnf sg t2 >>= fun t2' ->
-    if Reduction.are_convertible sg t1' t2'
-      then return true
-      else begin (*(fun pb -> Printf.eprintf "Unification: %a === %a\nunder %a\nwith %a.\n" pp_term t1 pp_term t2 pp_context (Context.to_context ctx) pp_problem pb; (),pb) >>= fun () ->*)
-      match t1', t2' with
-        | Meta (_,_,n,_), Meta (_,_,m,_) -> set_meta n t2' >>= (function | true -> return true | false -> set_meta m t1')
-        | Meta (_,_,n,_), _ -> set_meta n t2'
-        | _, Meta (_,_,n,_) -> set_meta n t1'
-        | Pi (l,x,a1,b1), Pi(_,_,a2,b2) -> aux ctx a1 a2 >>= fun b -> if b
-          then aux ((l,x,a1)::ctx) b1 b2
-          else return false
-        | _, _ -> (fun pb -> Printf.eprintf "Failed to unify %a === %a\nunder %a\nwith %a.\n" pp_term t1' pp_term t2' pp_context ctx pp_problem pb; (),pb) >>= fun () ->
-            return false
-    end in
+  let aux t1 t2 = gen_pair sg ctx t1 t2 >>= function
+    | Some [] -> return true
+    | Some l -> add_pairs l >>= fun () -> return true
+    | None -> return false
+  in
   match c with
-    | CTerm u -> aux ctx t u
+    | CTerm u -> aux t u
     | CType -> failwith "Refine mode: cannot unify with type"
     | CSort -> begin match t with
         | Kind | Type _ -> return true
-        | _ -> aux ctx t (mk_Type dloc) (* bad *)
+        | _ -> aux t (mk_Type dloc) (* bad *)
         end
 

d10e380adf249922710fecf0f82606afa50c0506
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2015-05-13 16:36:54 +0200

    No wrapper module for Msubst

--- kernel/msubst.ml
+++ kernel/msubst.ml
@@ -1,53 +1,52 @@
 open Basics
 open Term
 
-module S = struct
-  type t = term IntMap.t
-  
-  let identity = IntMap.empty
+type t = term IntMap.t
 
-  let subst_l l n t = Subst.psubst_l (LList.of_list (List.map Lazy.from_val l)) n t
+let identity = IntMap.empty
 
-  let meta_raw (sigma:t) n = try Some (IntMap.find n sigma) with | Not_found -> None
+let subst_l l n t = Subst.psubst_l (LList.of_list (List.map Lazy.from_val l)) n t
 
-  let meta_val (sigma:t) : term -> term option = function
-    | Meta (_,_,n,ts) -> begin
-      try let te0 = IntMap.find n sigma in
-        let subst1 = List.map snd ts in Some (subst_l subst1 0 te0)
-      with | Not_found -> None
+let meta_raw (sigma:t) n = try Some (IntMap.find n sigma) with | Not_found -> None
+
+let meta_val (sigma:t) : term -> term option = function
+  | Meta (_,_,n,ts) -> begin
+    try let te0 = IntMap.find n sigma in
+      let subst1 = List.map snd ts in Some (subst_l subst1 0 te0)
+    with | Not_found -> None
+    end
+  | _ -> None
+
+let apply (sigma:t) (t:term) : term =
+  let rec aux = function
+    | Kind | Type _ | Const _ | Hole _ | DB _ as t -> t
+    | App (f,a,args) -> mk_App (aux f) (aux a) (List.map (aux) args)
+    | Lam (l,x,Some a,te) -> mk_Lam l x (Some (aux a)) (aux te)
+    | Lam (l,x,None,te) -> mk_Lam l x None (aux te)
+    | Pi (l,x,a,b) -> mk_Pi l x (aux a) (aux b)
+    | Meta (lc,s,n,ts) as mt -> begin match meta_val sigma mt with
+        | Some mt' -> aux mt'
+        | None -> mk_Meta lc s n (List.map (fun (x,t) -> x,aux t) ts)
       end
-    | _ -> None
+  in if IntMap.is_empty sigma then t else aux t
 
-  let apply (sigma:t) (t:term) : term =
-    let rec aux = function
-      | Kind | Type _ | Const _ | Hole _ | DB _ as t -> t
-      | App (f,a,args) -> mk_App (aux f) (aux a) (List.map (aux) args)
-      | Lam (l,x,Some a,te) -> mk_Lam l x (Some (aux a)) (aux te)
-      | Lam (l,x,None,te) -> mk_Lam l x None (aux te)
-      | Pi (l,x,a,b) -> mk_Pi l x (aux a) (aux b)
-      | Meta (lc,s,n,ts) as mt -> begin match meta_val sigma mt with
-          | Some mt' -> aux mt'
-          | None -> mk_Meta lc s n (List.map (fun (x,t) -> x,aux t) ts)
-        end
-    in if IntMap.is_empty sigma then t else aux t
+let rec whnf sg sigma t = match Reduction.whnf sg t with
+  | Meta _ as mt -> begin match meta_val sigma mt with
+      | Some mt' -> whnf sg sigma mt'
+      | None -> mt
+      end
+  | App (Meta _ as mt, a, al) as t0 -> begin match meta_val sigma mt with
+      | Some mt' -> whnf sg sigma (mk_App mt' a al)
+      | None -> t0
+      end
+  | t0 -> t0
 
-  let rec whnf sg sigma t = match Reduction.whnf sg t with
-    | Meta _ as mt -> begin match meta_val sigma mt with
-        | Some mt' -> whnf sg sigma mt'
-        | None -> mt
-        end
-    | App (Meta _ as mt, a, al) as t0 -> begin match meta_val sigma mt with
-        | Some mt' -> whnf sg sigma (mk_App mt' a al)
-        | None -> t0
-        end
-    | t0 -> t0
+let add (sigma:t) (n:int) (t:term) : t =
+  assert ( not ( IntMap.mem n sigma ) );
+  IntMap.add n t sigma
 
-  let add (sigma:t) (n:int) (t:term) : t =
-    assert ( not ( IntMap.mem n sigma ) );
-    IntMap.add n t sigma
-  
-  let pp out sigma =
-    IntMap.iter
-      (fun i t -> Printf.fprintf out "( ?_%i |-> %a )" i pp_term t)
-      sigma
-end
+let pp out sigma =
+  IntMap.iter
+    (fun i t -> Printf.fprintf out "( ?_%i |-> %a )" i pp_term t)
+    sigma
+
--- kernel/msubst.mli
+++ kernel/msubst.mli
@@ -1,23 +1,23 @@
 (** Substitutions of metavariables *)
 open Term
 
-module S : sig
-  type t
-  
-  val identity : t
-  
-  val add : t -> int -> term -> t
-  
-  val apply : t -> term -> term
-  (** Recursive application. *)
-  
-  val meta_raw : t -> int -> term option
 
-  val meta_val : t -> term -> term option
-  (** If the term is a metavariable and has a value in the substitution we apply it, otherwise return None. *)
-  
-  val whnf : Signature.t -> t -> term -> term
-  (** Recursively apply Reduction.whnf and meta_val on the term's head. *)
-  
-  val pp : out_channel -> t -> unit
-end
+type t
+
+val identity : t
+
+val add : t -> int -> term -> t
+
+val apply : t -> term -> term
+(** Recursive application. *)
+
+val meta_raw : t -> int -> term option
+
+val meta_val : t -> term -> term option
+(** If the term is a metavariable and has a value in the substitution we apply it, otherwise return None. *)
+
+val whnf : Signature.t -> t -> term -> term
+(** Recursively apply Reduction.whnf and meta_val on the term's head. *)
+
+val pp : out_channel -> t -> unit
+

cd57b73cbe52bd197fd44fe4c1d4f78f5a20cb4b
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2015-06-01 18:01:56 +0200

    Fix subst.ml*

--- kernel/subst.ml
+++ kernel/subst.ml
@@ -1,24 +1,29 @@
 open Basics
 open Term
 
-let rec shift_rec (type a) (kind:a tkind) (r:int) (k:int) : a term -> a term = function
+let subst_handler (type a) (aux : a term -> a term) : a term -> a term = function
+  | Extra (lc,kind,ex) as t -> begin match kind with
+      | Untyped -> t
+      | Pretyped -> let { meta=(s,n,ts) } = ex in
+          mk_Meta lc s n (List.map (fun (x,t) -> x,aux t) ts)
+      | Typed -> t
+      end
+  | _ -> assert false
+
+let rec shift_rec (r:int) (k:int) : 'a term -> 'a term = function
   | DB (_,x,n) as t -> if n<k then t else mk_DB dloc x (n+r)
   | App (f,a,args) ->
-      mk_App (shift_rec kind r k f) (shift_rec kind r k a) (List.map (shift_rec kind r k) args )
-  | Lam (_,x,_,f) -> mk_Lam dloc x None (shift_rec kind r (k+1) f)
-  | Pi  (_,x,a,b) -> mk_Pi dloc x (shift_rec kind r k a) (shift_rec kind r (k+1) b)
-  | Extra (_,ex) -> begin match kind with
-      | Untyped -> mk_Extra dloc ex
-      | Pretyped -> let { meta=(s,n,ts) } = ex in
-          mk_Extra dloc (s,n,List.map (fun (x,t) -> x,shift_rec Pretyped r k t) ts)
-      | Typed -> ex.exfalso
-      end
+      mk_App (shift_rec r k f) (shift_rec r k a) (List.map (shift_rec r k) args )
+  | Lam (_,x,_,f) -> mk_Lam dloc x None (shift_rec r (k+1) f)
+  | Pi  (_,x,a,b) -> mk_Pi dloc x (shift_rec r k a) (shift_rec r (k+1) b)
+  | Extra _ as t -> subst_handler (fun t -> shift_rec r k t) t
   | Kind | Type _ | Const _ as t -> t
 
-let shift kind r t = shift_rec kind r 0 t
+let shift r t = shift_rec r 0 t
 
 exception UnshiftExn
-let unshift (type a) (kind:a tkind) q (te:a term) =
+
+let unshift q (te:'a term) : 'a term =
   let rec aux k = function
   | DB (_,_,n) as t when n<k -> t
   | DB (l,x,n) ->
@@ -28,81 +33,74 @@ let unshift (type a) (kind:a tkind) q (te:a term) =
   | Lam (l,x,None,f) -> mk_Lam l x None (aux (k+1) f)
   | Lam (l,x,Some a,f) -> mk_Lam l x (Some (aux k a)) (aux (k+1) f)
   | Pi  (l,x,a,b) -> mk_Pi l x (aux k a) (aux (k+1) b)
-  | Extra (l,ex) as t -> begin match kind with
-      | Pretyped -> let { meta=(s,n,ts) } = ex in
-          mk_Extra l (s,n,List.map (fun (x,t) -> x,aux k t) ts)
-      | Untyped | Typed -> t
-      end
+  | Extra _ as t -> subst_handler (aux k) t
   | Type _ | Kind | Const _ as t -> t
   in
     aux 0 te
 
-let rec psubst_l (type a) (kind:a tkind) (args:(a term Lazy.t) LList.t) (k:int) (t:a term) : a term =
+let rec psubst_l (args:('a term Lazy.t) LList.t) (k:int) (t:'a term) : 'a term =
   let nargs = args.LList.len in
   match t with
     | Type _ | Kind | Const _ -> t
     | DB (_,x,n) when (n >= (k+nargs))  -> mk_DB dloc x (n-nargs)
     | DB (_,_,n) when (n < k)           -> t
     | DB (_,_,n) (* (k<=n<(k+nargs)) *) ->
-        shift kind k ( Lazy.force (LList.nth args (n-k)) )
+        shift k ( Lazy.force (LList.nth args (n-k)) )
     | Lam (_,x,_,b)                     ->
-        mk_Lam dloc x None (psubst_l kind args (k+1) b)
+        mk_Lam dloc x None (psubst_l args (k+1) b)
     | Pi  (_,x,a,b)                     ->
-        mk_Pi dloc x (psubst_l kind args k a) (psubst_l kind args (k+1) b)
+        mk_Pi dloc x (psubst_l args k a) (psubst_l args (k+1) b)
     | App (f,a,lst)                     ->
-        mk_App (psubst_l kind args k f) (psubst_l kind args k a)
-          (List.map (psubst_l kind args k) lst)
-    | Extra (_,ex) as t -> begin match kind with
-        | Pretyped -> let { meta=(s,n,ts) } = ex in
-            mk_Extra dloc (s,n,List.map (fun (x,t) -> x,psubst_l kind args k t) ts)
-        | Untyped | Typed -> t
-        end
+        mk_App (psubst_l args k f) (psubst_l args k a)
+          (List.map (psubst_l args k) lst)
+    | Extra _ as t -> subst_handler (psubst_l args k) t
 
-let subst (type a) (kind:a tkind) (te:a term) (u:a term) =
+let subst (te:'a term) (u:'a term) =
   let rec aux k = function
     | DB (l,x,n) as t ->
-        if n = k then shift kind k u
+        if n = k then shift k u
         else if n>k then mk_DB l x (n-1)
         else (*n<k*) t
     | Type _ | Kind | Const _ as t -> t
     | Lam (_,x,_,b) -> mk_Lam dloc x None (aux (k+1) b)
-    | Pi  (_,x,a,b) -> mk_Pi dloc  x (aux k a) (aux(k+1) b)
+    | Pi  (_,x,a,b) -> mk_Pi dloc  x (aux k a) (aux (k+1) b)
     | App (f,a,lst) -> mk_App (aux k f) (aux k a) (List.map (aux k) lst)
-    | Extra (_,ex) as t -> begin match kind with
-        | Pretyped -> let { meta=(s,n,ts) } = ex in
-            mk_Meta dloc (s,n,List.map (fun (x,t) -> x,aux k t) ts)
-        | Untyped | Typed -> t
-        end
+    | Extra _ as t -> subst_handler (aux k) t
   in aux 0 te
 
 
 module S =
 struct
-  type 'a t = 'a tkind*(ident*'a term) IntMap.t
-  let identity k = k,IntMap.empty
+  type 'a t = (ident*'a term) IntMap.t
 
-  let apply (type a) (kind,sigma:a t) (te:a term) (q:int) : a term =
+  let identity = IntMap.empty
+
+  let apply (sigma:'a t) (te:'a term) (q:int) : 'a term =
     let rec aux q = function
       | Kind | Type _ | Const _ as t -> t
       | DB (_,_,k) as t when k<q -> t
       | DB (_,_,k) as t (*when k>=q*) ->
           begin
-            try shift kind q (snd (IntMap.find (k-q) sigma))
+            try shift q (snd (IntMap.find (k-q) sigma))
             with Not_found -> t
           end
       | App (f,a,args) -> mk_App (aux q f) (aux q a) (List.map (aux q) args)
       | Lam (l,x,Some ty,te) -> mk_Lam l x (Some (aux q ty)) (aux (q+1) te)
       | Lam (l,x,None,te) -> mk_Lam l x None (aux (q+1) te)
       | Pi (l,x,a,b) -> mk_Pi l x (aux q a) (aux (q+1) b)
-      | Extra (l,ex) as t -> begin match kind with
-          | Pretyped -> let { meta=(s,n,ts) } = ex in
-              mk_Extra l (s,n,List.map (fun (x,t) -> x,aux q t) ts)
-          | Untyped | Typed -> t
-          end
+      | Extra _ as t -> subst_handler (aux q) t
     in
       aux q te
 
-  let occurs (type a) (kind:a tkind) (n:int) (te:a term) : bool =
+  let occurs_handler (type a) (aux:a term -> bool) : a term -> bool = function
+    | Extra (_,kind,ex) -> begin match kind with
+      | Untyped -> false
+      | Pretyped -> let {meta=(_,_,ts)} = ex in List.exists (fun (_,t) -> aux t) ts
+      | Typed -> false
+      end
+    | _ -> assert false
+
+  let occurs (n:int) (te:'a term) : bool =
     let rec aux q = function
       | Kind | Type _ | Const _ -> false
       | DB (_,_,k) when k<q -> false
@@ -111,29 +109,25 @@ struct
       | Lam (_,_,None,te) -> aux (q+1) te
       | Lam (_,_,Some ty,te) -> aux q ty || aux (q+1) te
       | Pi (_,_,a,b) -> aux q a || aux (q+1) b
-      | Extra (_,ex) -> begin match kind with
-          | Pretyped -> let { meta=(_,_,ts) } = ex in
-              List.exists (fun (_,t) -> aux q t) ts
-          | Untyped | Typed -> false
-          end
+      | Extra _ as t -> occurs_handler (aux q) t
     in aux 0 te
 
-  let add (kind,sigma:'a t) (x:ident) (n:int) (t:'a term) : 'a t option =
+  let add (sigma:'a t) (x:ident) (n:int) (t:'a term) : 'a t option =
     assert ( not ( IntMap.mem n sigma ) );
-    if occurs kind 0 t then None
-    else Some (kind, IntMap.add n (x,t) sigma)
+    if occurs 0 t then None
+    else Some (IntMap.add n (x,t) sigma)
 
-  let merge (kind,s1) (_,s2) =
+  let merge s1 s2 =
     let aux _ b1 b2 = match b1, b2 with
       | None, b | b, None -> b
       | Some b1, Some b2 -> assert false (*FIXME*)
     in
-      kind,IntMap.merge aux s1 s2
+      IntMap.merge aux s1 s2
 
-  let is_identity (_,s) = IntMap.is_empty s
+  let is_identity = IntMap.is_empty
 
-  let pp (out:out_channel) (kind,sigma:'a t) : unit =
+  let pp (out:out_channel) (sigma:'a t) : unit =
     IntMap.iter (fun i (x,t) ->
-        Printf.fprintf out "( %a[%i] = %a )" pp_ident x i (pp_term kind) t
+        Printf.fprintf out "( %a[%i] = %a )" pp_ident x i pp_term t
       ) sigma
 end
--- kernel/subst.mli
+++ kernel/subst.mli
@@ -1,21 +1,20 @@
 (** Substitutions using DeBruijn indices. *)
 open Term
 
-val shift               : 'a tkind -> int -> 'a term -> 'a term
+val shift               : int -> 'a term -> 'a term
 exception UnshiftExn
-val unshift             : 'a tkind -> int -> 'a term -> 'a term
+val unshift             : int -> 'a term -> 'a term
 
-val psubst_l            : 'a tkind -> ('a term Lazy.t) Basics.LList.t -> int -> 'a term -> 'a term
+val psubst_l            : ('a term Lazy.t) Basics.LList.t -> int -> 'a term -> 'a term
 (** Parallel substitution of lazy terms. *)
 
-val subst               : 'a tkind -> 'a term -> 'a term -> 'a term
+val subst               : 'a term -> 'a term -> 'a term
 (** [subst te u] substitutes the deBruijn indice [0] with [u] in [te]. *)
 
 module S :
 sig
   type 'a t
-  val identity : 'a tkind -> 'a t
-  val kind : 'a t -> 'a tkind
+  val identity : 'a t
   val add : 'a t -> Basics.ident -> int -> 'a term -> 'a t option
   val apply : 'a t -> 'a term -> int -> 'a term
   val merge : 'a t -> 'a t -> 'a t

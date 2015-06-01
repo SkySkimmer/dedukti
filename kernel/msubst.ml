2c377a753eca90ea4001b3bd6b4018d06bb67cb8
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2015-06-01 15:09:15 +0200

    Modified msubst.ml* for parametric terms (1st pass)

--- kernel/msubst.ml
+++ kernel/msubst.ml
@@ -1,15 +1,15 @@
 open Basics
 open Term
 
-type t = term IntMap.t
+type t = pretyped term IntMap.t
 
 let identity = IntMap.empty
 
-let subst_l l n t = Subst.psubst_l (LList.of_list (List.map Lazy.from_val l)) n t
+let subst_l l n t = Subst.psubst_l Pretyped (LList.of_list (List.map Lazy.from_val l)) n t
 
 let meta_raw (sigma:t) n = try Some (IntMap.find n sigma) with | Not_found -> None
 
-let meta_val (sigma:t) : term -> term option = function
+let meta_val (sigma:t) : pretyped term -> pretyped term option = function
   | Meta (_,_,n,ts) -> begin
     try let te0 = IntMap.find n sigma in
       let subst1 = List.map snd ts in
@@ -19,20 +19,32 @@ let meta_val (sigma:t) : term -> term option = function
     end
   | _ -> None
 
-let apply (sigma:t) (t:term) : term =
+let apply (sigma:t) (t:pretyped term) : pretyped term =
   let rec aux = function
-    | Kind | Type _ | Const _ | Hole _ | DB _ as t -> t
+    | Kind | Type _ | Const _ | DB _ as t -> t
     | App (f,a,args) -> mk_App (aux f) (aux a) (List.map (aux) args)
     | Lam (l,x,Some a,te) -> mk_Lam l x (Some (aux a)) (aux te)
     | Lam (l,x,None,te) -> mk_Lam l x None (aux te)
     | Pi (l,x,a,b) -> mk_Pi l x (aux a) (aux b)
-    | Meta (lc,s,n,ts) as mt -> begin match meta_val sigma mt with
+    | Extra (lc,{meta=(s,n,ts)}) as mt -> begin match meta_val sigma mt with
         | Some mt' -> aux mt'
-        | None -> mk_Meta lc s n (List.map (fun (x,t) -> x,aux t) ts)
-      end
+        | None -> mk_Extra lc (s,n,(List.map (fun (x,t) -> x,aux t) ts))
+        end
   in if IntMap.is_empty sigma then t else aux t
 
-let rec whnf sg sigma t = match Reduction.whnf sg t with
+let to_ground (sigma:t) (t:pretyped term) : typed term =
+  let rec aux = function
+    | Kind | Type _ | DB _ | Const _ as t -> t
+    | App (f,a,args) -> mk_App (aux f) (aux a) (aux args)
+    | Lam (l,x,Some a,te) -> mk_Lam l x (Some (aux a)) (aux te)
+    | Lam (l,x,None,te) -> mk_Lam l x None (aux te)
+    | Pi (l,x,a,b) -> mk_Pi l x (aux a) (aux b)
+    | Extra (lc,{meta=(s,n,ts)}) as mt -> begin match meta_val sigma mt with
+        | Some mt' -> aux mt'
+        | None -> None
+        end
+
+let rec whnf sg sigma t = match Reduction.whnf Pretyped sg t with
   | Meta _ as mt -> begin match meta_val sigma mt with
       | Some mt' -> whnf sg sigma mt'
       | None -> mt
@@ -45,7 +57,7 @@ let rec whnf sg sigma t = match Reduction.whnf sg t with
 
 let mem sigma n = IntMap.mem n sigma
 
-let add (sigma:t) (n:int) (t:term) : t =
+let add (sigma:t) (n:int) (t:pretyped term) : t =
   assert ( not ( IntMap.mem n sigma ) );
   IntMap.add n t sigma
 
--- kernel/msubst.mli
+++ kernel/msubst.mli
@@ -8,17 +8,18 @@ val identity : t
 
 val mem : t -> int -> bool
 
-val add : t -> int -> term -> t
+val add : t -> int -> pretyped term -> t
 
-val apply : t -> term -> term
-(** Recursive application. *)
+val apply : t -> pretyped term -> pretyped term
 
-val meta_raw : t -> int -> term option
+val to_ground : t -> pretyped term -> typed term option
 
-val meta_val : t -> term -> term option
+val meta_raw : t -> int -> pretyped term option
+
+val meta_val : t -> pretyped term -> pretyped term option
 (** If the term is a metavariable and has a value in the substitution we apply it, otherwise return None. *)
 
-val whnf : Signature.t -> t -> term -> term
+val whnf : Signature.t -> t -> pretyped term -> pretyped term
 (** Recursively apply Reduction.whnf and meta_val on the term's head. *)
 
 val normalize : t -> t
